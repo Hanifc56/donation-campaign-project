0d20a47b9d3ce85c7ea4ca63493a6ae7508a7bae
Hanifc56 <abuhanif007sd@gmail.com>
2023-12-18 22:56:45 +0600

    bg added dynamically solved  error

--- src/Components/DCard/Dcard.jsx
+++ src/Components/DCard/Dcard.jsx
@@ -1,12 +1,16 @@
 const Dcard = ({ card }) => {
-  const { id, picture } = card;
+  const { id, picture, card_bg } = card;
+
   return (
     <div className="">
       <div className="card mx-auto card-compact w-[300px] bg-base-100 shadow-xl">
         <figure>
           <img src={picture} alt="" />
         </figure>
-        <div className="card-body">
+        <div
+          style={{ backgroundColor: card_bg }}
+          className="card-body rounded-b-lg"
+        >
           <h2 className="card-title">Shoes!</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
         </div>
