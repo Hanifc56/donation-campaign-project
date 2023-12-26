aaa7782f8413214228f123a2e04a8cf98561f218
Hanifc56 <abuhanif007sd@gmail.com>
2023-12-26 23:15:40 +0600

    search feature added finally

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,54 +2,6 @@ import { useEffect, useState } from "react";
 import DonationCategory from "../DonationCategory/DonationCategory";
 import Header from "../Header/Header";
 
-// const Home = () => {
-//   const [donationCard, setDonationCard] = useState([]);
-//   const [displayCards, setDisplayCards] = useState([]);
-
-//   const [text, setText] = useState("");
-//   console.log(displayCards);
-
-//   const handleCardCategory = (filter) => {
-//     let filteredCards = [];
-
-//     if (filter === "") {
-//       filteredCards = displayCards.length ? displayCards : donationCard;
-//     } else {
-//       filteredCards = donationCard.filter((card) =>
-//         card.category.toLowerCase().includes(filter.toLowerCase())
-//       );
-//     }
-//     setDisplayCards(filteredCards);
-//   };
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     setText(e.target.name.value);
-//     console.log(text);
-//   };
-//   const handleChange = (e) => {
-//     setText(e.target.value);
-//   };
-
-//   useEffect(() => {
-//     fetch("data.json")
-//       .then((res) => res.json())
-//       .then((data) => {
-//         setDonationCard(data);
-//         setDisplayCards(data);
-//       });
-//   }, []);
-//   return (
-//     <div>
-//       <Header
-//         handleChange={handleChange}
-//         handleSubmit={handleSubmit}
-//         handleCardCategory={handleCardCategory}
-//       ></Header>
-//       <DonationCategory donationCard={displayCards}></DonationCategory>
-//     </div>
-//   );
-// };
-
 const Home = () => {
   const [donationCard, setDonationCard] = useState([]);
   const [displayCards, setDisplayCards] = useState([]);
@@ -59,10 +11,10 @@ const Home = () => {
     if (filter === "") {
       setDisplayCards(donationCard);
     } else {
-      const filteredCards = donationCard.filter(
+      const searchCards = donationCard.filter(
         (card) => card.category.toLowerCase() === filter.toLowerCase()
       );
-      setDisplayCards(filteredCards);
+      setDisplayCards(searchCards);
     }
   };
 
@@ -71,6 +23,7 @@ const Home = () => {
     const category = e.target.name.value;
     setText(category);
     handleCardCategory(category);
+    console.log(text);
   };
 
   const handleChange = (e) => {
