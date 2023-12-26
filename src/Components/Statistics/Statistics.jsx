0d25863ea8642565d3b1d6a4793acee71060a56b
Hanifc56 <abuhanif007sd@gmail.com>
2023-12-27 00:52:27 +0600

    pie chart created

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,7 +1,63 @@
+import { PieChart, Pie, Sector, Cell, Legend, Tooltip } from "recharts";
+
+const data = [
+  { name: "Total Donation", value: 400 },
+  { name: "Your Donation", value: 300 },
+];
+const COLORS = ["#FF444A", "#00C49F", "#FFBB28", "#FF8042"];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
 const Statistics = () => {
   return (
-    <div>
-      <h1>Statistics</h1>
+    <div className="max-w-7xl mx-auto flex w-100">
+      <div className="pt-32 mx-auto">
+        <PieChart width={400} height={400}>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <Legend></Legend>
+          <Tooltip></Tooltip>
+        </PieChart>
+      </div>
     </div>
   );
 };
